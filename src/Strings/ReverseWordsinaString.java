3914dcefea227b2421160e54760591478e3fcad5
sarvesh <srbhanarkar05@gmail.com>
2026-02-06 20:15:20 +0530

    added stringg

--- src/Strings/ReverseWordsinaString.java
+++ src/Strings/ReverseWordsinaString.java
@@ -2,7 +2,7 @@ package Strings;
 
 public class ReverseWordsinaString {
     public static void main(String[] args) {
-        String s = "  hello world  ";
+        String s = "   hello world  ";
         System.out.println(reverseWords(s));
     }
 
