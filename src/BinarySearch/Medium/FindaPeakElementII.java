fc0478db41f1761423f1d2f684d758301c20d76c
sarvesh <srbhanarkar05@gmail.com>
2026-02-06 20:15:03 +0530

    added stringg

---
+++ src/BinarySearch/Medium/FindaPeakElementII.java
@@ -0,0 +1,10 @@
+//package BinarySearch.Medium;
+//
+//public class FindaPeakElementII {
+//    static void main(String[] args) {
+//
+//    }
+//
+//    static public int[] findPeakGrid(int[][] mat) {
+//
+//    }
---
+++ src/Strings/ReverseWordsinaString.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class ReverseWordsinaString {
+    public static void main(String[] args) {
+        String s = "  hello world  ";
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+
+        int left = 0, right = words.length - 1;
+        while (left < right) {
+            String temp = words[left];
+            words[left] = words[right];
+            words[right] = temp;
+            left++;
+            right--;
+        }
+
+        return String.join(" ", words);
+    }
+}
