6501916cd479d8e2103f50058e3014211ed51d87
sarvesh <srbhanarkar05@gmail.com>
2026-02-09 20:07:13 +0530

    added stringg

---
+++ src/Strings/Easy/LongestCommonPrefixs.java
@@ -0,0 +1,16 @@
+//package Strings.Easy;
+//
+//public class LongestCommonPrefixs {
+//    static void main(String[] args) {
+//        String[] strs = {"flower","flow","flight"};
+////        System.out.println(LongestCommonPrefix(strs));
+//    }
+//
+//    static public LongestCommonPrefix(String[] args){
+//
+//
+//
+//
+//        return "";
+//    }
+//}
