0a7d3ed84bfcf8bc6df8747bc70e2606e0d5ca75
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2026-06-12 16:30:00 +0530

    Time: 29 ms (32.40%) | Memory: 46.6 MB (31.32%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,21 +1,116 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-       int n =nums.length;
-       long result =0;
 
-        for(int i =0;i<n;i++){
-            int min=nums[i];
-            int max=nums[i];
+    public long subMin(int[] nums){
+        int n = nums.length;
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<>();
 
-            for(int j=i;j<n;j++){
-                max=Math.max(max,nums[j]);
-                min=Math.min(min,nums[j]);
+        for(int i = n-1;i>=0;i--){
 
-                int sum=max-min;
-                result+=sum;
+            while(!st.isEmpty() && nums[st.peek()]>= nums[i]){
+                st.pop();
             }
+            if(st.isEmpty()){
+                nse[i]=n;
+            }
+            else{
+                nse[i]=st.peek();
+            }
+
+            st.push(i);
         }
 
-        return result;
+        st.clear();
+
+        int[] psee = new int[n];
+
+        for(int i = 0;i<n;i++){
+
+            while(!st.isEmpty() && nums[st.peek()]> nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                psee[i]=-1;
+            }
+            else{
+                psee[i]=st.peek();
+            }
+
+            st.push(i);
+        }
+
+        long answer=0;
+
+        for (int i = 0; i < n; i++) {
+
+            long left = i - psee[i];
+            long right = nse[i] - i;
+
+            long contri = left * right * nums[i];
+
+           answer += contri;
+        }
+
+        return answer;
+
+    }
+
+    public long subMax(int[] nums){
+        int n = nums.length;
+        int[] nge = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = n-1;i>=0;i--){
+
+            while(!st.isEmpty() && nums[st.peek()]<= nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nge[i]=n;
+            }
+            else{
+                nge[i]=st.peek();
+            }
+
+            st.push(i);
+        }
+
+        st.clear();
+
+        int[] pgee = new int[n];
+
+        for(int i = 0;i<n;i++){
+
+            while(!st.isEmpty() && nums[st.peek()]< nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                pgee[i]=-1;
+            }
+            else{
+                pgee[i]=st.peek();
+            }
+
+            st.push(i);
+        }
+
+        long answer=0;
+
+        for (int i = 0; i < n; i++) {
+
+            long left = i - pgee[i];
+            long right = nge[i] - i;
+
+            long contri = left * right * nums[i];
+
+           answer += contri;
+        }
+
+        return answer;
+
+    }
+
+    public long subArrayRanges(int[] nums) {
+        return (subMax(nums)-subMin(nums));
     }
 }
