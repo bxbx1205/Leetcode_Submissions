7e68983791ec87d173c401a0e0ebb6f0a5fea1d7
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2026-06-15 16:47:04 +0530

    Time: 117 ms (5.80%) | Memory: 76.1 MB (94.69%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,59 +1,57 @@
 class Solution {
-    
-    public static int[] pse(int[] arr) {
-        int n = arr.length;
-        Stack<Integer> st = new Stack();
-        int[] ans = new int[n];
-        
-        for(int i = 0 ;i<n ;i++){
-            
-            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+    public int[] pse(int[] nums){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[nums.length];
+
+        for(int i =0;i<nums.length;i++){
+
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
                 st.pop();
             }
-            
+
             if(st.isEmpty()){
                 ans[i]=-1;
             }
             else{
                 ans[i]=st.peek();
             }
-            
             st.push(i);
         }
-        
+
         return ans;
     }
 
-    public static int[] nse(int[] arr) { 
-    int n = arr.length; 
-    Stack<Integer> st = new Stack<>(); 
-    int[] ans = new int[n]; 
-    
-    for(int i = n - 1; i >= 0; i--){ 
-        while(!st.isEmpty() && arr[st.peek()] >= arr[i]){ 
-            st.pop(); 
-        } 
-        
-        if(st.isEmpty()){ 
-            ans[i] = n; 
-        } else { 
-            ans[i] = st.peek(); 
-        } 
-        
-        st.push(i); 
-    } 
-    return ans; 
-    }
-    public int largestRectangleArea(int[] heights) {
-        int[] nse = nse(heights);
-        int[] pse = pse(heights);
-        int area=0;
+    public int[] nse(int[] nums){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[nums.length];
 
-        for(int i=0;i<heights.length;i++){
-            int current= heights[i]*(nse[i]-pse[i]-1);
-            area=Math.max(area,current);
+        for(int i =nums.length-1;i>=0;i--){
+
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans[i]=nums.length;
+            }
+            else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
         }
 
+        return ans;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int[] pse = pse(heights);
+        int[] nse = nse(heights);
+        int area =0;
+
+        for(int i =0;i<heights.length;i++){
+            int current = heights[i]*(nse[i]-pse[i]-1);
+            area=Math.max(area,current);
+        }
 
         return area;
     }
