92c4db55bec8d93864d73d8713cd7f085f05ae70
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2026-06-26 15:03:21 +0530

    Time: 4 ms (48.45%) | Memory: 50.5 MB (87.79%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,50 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    public int goal(int[] nums, int goal){
+        if(goal<0) return 0;
+        int left=0;
+        int right=0;
+        int n = nums.length;
+        int sum=0;
         int cnt=0;
-        int n =nums.length;
 
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                int current=nums[j];
-                sum+=current;
-                if(sum==goal){
-                    cnt++;
-                }
+        while(right<nums.length){
+            sum+=nums[right];
+
+            while(sum>goal){
+                sum-=nums[left];
+                left++;
             }
+            cnt=cnt+(right-left+1);
+            right++;
         }
 
         return cnt;
+    }
+
+    public int goalm1(int[] nums, int goal){
+        if(goal<0) return 0;
+        int left=0;
+        int right=0;
+        int n = nums.length;
+        int sum=0;
+        int cnt=0;
+
+        while(right<nums.length){
+            sum+=nums[right];
+
+            while(sum>goal){
+                sum-=nums[left];
+                left++;
+            }
+            cnt=cnt+(right-left+1);
+            right++;
+        }
+
+        return cnt;
+    }
+
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return goal(nums,goal)-goalm1(nums,goal-1);
     }
 }
